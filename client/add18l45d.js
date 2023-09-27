30be111daba82cb1740bcc3a36f97ce0c83f110c
Ryan W <ryans09sstc@gmail.com>
2023-09-27 08:23:09 -0500

    Updates

---
+++ client/add18l45d.js
@@ -0,0 +1,10 @@
+export function add18l45d() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+
+  var htmlSelect = document.getElementById('lstBox1');
+  var text = "18L45D"
+  var dvalue = "18L45D"
+  htmlSelect.add(new Option(text, dvalue))
+
+  }
---
+++ client/add18r45d.js
@@ -0,0 +1,10 @@
+export function add18r45d() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+
+var htmlSelect = document.getElementById('lstBox1');
+var text = "18R45D"
+var dvalue = "18R45D"
+htmlSelect.add(new Option(text, dvalue))
+
+}
